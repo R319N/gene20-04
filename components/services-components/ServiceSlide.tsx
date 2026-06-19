b33430f5f88425a9e7ac3a3f5f027500fb6ad20d
Wilfred Reign <75909703+R319N@users.noreply.github.com>
2026-06-19 18:39:15 +0200

    Comment out Box component in ServiceSlide
    Commented out the Box component for slide image wrapping.

--- components/services-components/ServiceSlide.tsx
+++ components/services-components/ServiceSlide.tsx
@@ -414,7 +414,7 @@ const ServiceSlide: React.FC<props> = ({ image, name, detailIntro, deliverables,
                     />
                 </Box>
 
-                <Box
+              {/*  <Box
                     className="slide-image-wrap slide-animate"
                     sx={{
                         position: "relative",
