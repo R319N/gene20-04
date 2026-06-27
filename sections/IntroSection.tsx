ad4383604aecf8d00603e24490601b7911ac033a
Wilfred Reign <wilfredr319n@gmail.com>
2026-06-27 06:40:46 +0200

    new

--- sections/IntroSection.tsx
+++ sections/IntroSection.tsx
@@ -29,7 +29,6 @@ const IntroSection = () => {
       }}
     >
       <Box
-<<<<<<< HEAD
         sx={{
           position: "absolute",
           inset: 0,
@@ -42,18 +41,6 @@ const IntroSection = () => {
           <div className="intro-gradient" />
         </div>
       </Box>
-=======
-  sx={{
-    position: "absolute",
-    inset: 0,
-    // overflowX: "hidden",
-    pointerEvents: "none",
-    zIndex: 0,
-  }}
->
-  <Box className="intro-gradient" />
-</Box>
->>>>>>> 0aabe8477dd799b726906bc252921b3640414faf
 
       <Box
         sx={{
@@ -63,11 +50,7 @@ const IntroSection = () => {
           inset: 0,
           opacity: { xs: 0.46, md: 0.2 },
           pointerEvents: 'none',
-<<<<<<< HEAD
           overflow: "hidden"
-=======
-          overflow:"hidden"
->>>>>>> 0aabe8477dd799b726906bc252921b3640414faf
         }}
       >
         <MountainField3D />
