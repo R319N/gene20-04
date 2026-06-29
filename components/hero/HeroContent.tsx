0fc7f8a00fc7aa9e15b6df58a1ba177b4c72de76
Wilfred Reign <75909703+R319N@users.noreply.github.com>
2026-06-29 10:35:32 +0200

    Update hero content text to 'Digital Excellence Redefined'

--- components/hero/HeroContent.tsx
+++ components/hero/HeroContent.tsx
@@ -37,7 +37,7 @@ const HeroContent = () => {
                     textTransform: "uppercase"
                 }}
             >
-                Where Vision Becomes Reality
+                Digital Excellence Redefined
             </Typography>
 
             <Typography
